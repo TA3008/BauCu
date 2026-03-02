97c5750ff9aab300a1119ed74d94b32b293253f0
Ta3008 <nguyentuananh30082002@gmail.com>
2026-03-02 23:59:27 +0700

    update: allow host from ngrok

--- baucu/settings.py
+++ baucu/settings.py
@@ -17,8 +17,8 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True').lower() in ('true', '1', 'yes')
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
-
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1', 'https://krystyna-unecstatic-electrophonically.ngrok-free.dev').split(',')
+    
 # ---------------------------------------------------------------------------
 # Application definition
 # ---------------------------------------------------------------------------
